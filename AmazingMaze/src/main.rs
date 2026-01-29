f92b61ac155093d05bdccf06f0fab8cbf86bd3b0
Maxime Bodin <maxime.bodin@mailo.com>
2026-01-29 02:47:10 +0100

    Amazing Maze WIP : Première version de explore.

--- AmazingMaze/src/main.rs
+++ AmazingMaze/src/main.rs
@@ -1,3 +1,5 @@
+use crate::Maze::{Branch, Leaf};
+
 enum Exploration {
     UnExplored,
     Explored,
@@ -8,30 +10,54 @@ enum Maze<'a> {
     Leaf { label: String },
 }
 
+impl<'a> Maze<'a> {
+    fn explore(&self, trace: &mut Vec<String>) {
+        match self {
+            Leaf { label } => trace.push(label.to_string()),
+            Branch { label, left, right } => {
+                trace.push(label.to_string());
+                left.explore(trace);
+                right.explore(trace);
+            }
+        }
+    }
+}
+
 fn leaf(label: &str) -> Maze {
-    Maze::Leaf { label: label.to_string() }
+    Leaf { label: label.to_string() }
 }
 
 fn branch<'a>(label: &str, left: &'a Maze<'a>, right: &'a Maze<'a>) -> Maze<'a> {
-    Maze::Branch { label: label.to_string(), left, right }
+    Branch { label: label.to_string(), left, right }
 }
 
 fn main() {
-    let leaf4 = leaf("4");
     let leaf5 = leaf("5");
-    let maze3 = branch("3", &leaf4, &leaf5);
+    let leaf4 = leaf("4");
+    let branch3 = branch("3", &leaf4, &leaf5);
 
+    let leaf2 = &leaf("2");
+    let leaf8 = &leaf("8");
 
-    let my_maze = branch("0",
-                         &branch("1",
-                                 &leaf("2"),
-                                 &maze3),
-                         &branch("6",
-                                 &maze3,
-                                 &branch("7",
-                                         &leaf5,
-                                         &leaf("8"),
-                                 ),
-                         ),
+    let branch1 = &branch("1", leaf2, &branch3);
+
+    let branch7 = &branch("7", &leaf5, leaf8);
+    let branch6 = &branch("6", &branch3, branch7);
+
+    let my_maze = branch("0", branch1, branch6);
+
+    let mut trace: Vec<String> = vec![];
+    my_maze.explore(&mut trace);
+
+    println!("{:?}", trace);
+
+    assert_eq!(
+            trace,
+            vec!["0", "1", "2", "3", "4", "5", "6", "3", "4", "5", "7", "5", "8"],
     );
+
+    assert_eq!(
+            trace,
+            vec!["0", "1", "2", "3", "4", "5", "6", "3", "7", "5", "8"],
+        );
 }
